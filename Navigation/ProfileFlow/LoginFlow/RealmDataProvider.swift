2e908b9149331cc5a4be0a87e26c62aca7e3635a
Evgenii Bezgin <bezgineg@yandex.ru>
2021-06-04 20:35:56 +0300

    Add realm encryption

--- Navigation/ProfileFlow/LoginFlow/RealmDataProvider.swift
+++ Navigation/ProfileFlow/LoginFlow/RealmDataProvider.swift
@@ -16,7 +16,13 @@ final class RealmDataProvider: DataProvider {
     
     private var realm: Realm? {
         
-        var config = Realm.Configuration(encryptionKey: getKey())
+        var key = Data(count: 64)
+        
+        var _ = key.withUnsafeMutableBytes { (pointer: UnsafeMutableRawBufferPointer) in
+            SecRandomCopyBytes(kSecRandomDefault, 64, pointer.baseAddress!) }
+        
+        var config = Realm.Configuration(encryptionKey: key)
+
         config.fileURL = config.fileURL?.deletingLastPathComponent().appendingPathComponent("EncryptedCredentials.realm")
         return try? Realm(configuration: config)
     }
@@ -45,38 +51,6 @@ final class RealmDataProvider: DataProvider {
         try? realm?.write {
             realm?.delete(cachedCredential)
         }
-    }
-    
-    func getKey() -> Data {
-        let keychainIdentifier = "io.Realm.EncryptionExampleKey"
-        let keychainIdentifierData = keychainIdentifier.data(using: String.Encoding.utf8, allowLossyConversion: false)!
-        var query: [NSString: AnyObject] = [
-            kSecClass: kSecClassKey,
-            kSecAttrApplicationTag: keychainIdentifierData as AnyObject,
-            kSecAttrKeySizeInBits: 512 as AnyObject,
-            kSecReturnData: true as AnyObject
-        ]
-
-        var dataTypeRef: AnyObject?
-        var status = withUnsafeMutablePointer(to: &dataTypeRef) { SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0)) }
-        if status == errSecSuccess {
-            return dataTypeRef as! Data
-        }
-
-        var key = Data(count: 64)
-        key.withUnsafeMutableBytes({ (pointer: UnsafeMutableRawBufferPointer) in
-            let result = SecRandomCopyBytes(kSecRandomDefault, 64, pointer.baseAddress!)
-            assert(result == 0, "Failed to get random bytes")
-        })
-        query = [
-            kSecClass: kSecClassKey,
-            kSecAttrApplicationTag: keychainIdentifierData as AnyObject,
-            kSecAttrKeySizeInBits: 512 as AnyObject,
-            kSecValueData: key as AnyObject
-        ]
-        status = SecItemAdd(query as CFDictionary, nil)
-        assert(status == errSecSuccess, "Failed to insert the new key in the keychain")
-        return key
     }
 }
     
